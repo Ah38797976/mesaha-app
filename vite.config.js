1b85ae4fef9d83950dc2f99d18db0a66ac8f0453
Ah38797976 <ahmedsidiatah@gmail.com>
2026-08-18 05:37:08 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          supabase: ['@supabase/supabase-js'],
+        },
+      },
+    },
+  },
+});
